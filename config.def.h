70b82df48c194438dd7d0b9e57954bebe00efd2c
Marcus Kolber <marcus.kolber@gmail.com>
2023-05-15 09:21:38 -0400

    Inconsolata Nerd Font Mono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "InconsolataNerdFontMono-Regular:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Inconsolata Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
+
 /* Spare fonts */
 static char *font2[] = {
     "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true",
